49bff9f42ae10a6b5f424999c534aa9a6b4ed69a
Xzero <xzero@Xzeros-MacBook-Pro.local>
2020-11-24 16:08:36 +0400

    Added changes and Removed upload folder and qrcode folder from git

--- api/vouchers/config/schema.graphql.js
+++ api/vouchers/config/schema.graphql.js
@@ -1,68 +1,58 @@
 module.exports = {
-    definition: `
-      type UserVoucher {
-        disabled: Boolean,
-        won: Boolean!,
-        voucher: Vouchers
-      }
-
-      type AvailableVouchers {
-        vouchers: [Vouchers],
-        AvailedVouchers: [VoucherAvailed]!,
-
-      }
-
-    `,
-
-    mutation: `
-        GenerateVoucherWinner(user_id: Int!, plan_id: Int!): UserVoucher!,
-        
-        FinalizeWinner: VoucherAvailed!,
-        
-    `,
-
-
-    query: 'AvailableVouchers(where: JSON): AvailableVouchers!',
-
-    resolver: {
-      Mutation: {
-        GenerateVoucherWinner: {
-          description: 'Randomly selecting voucher winner with their respective plans',
-          policies: [],
-          resolverOf: 'application::vouchers.vouchers.find',
-          resolver: async (obj, options, ctx) => {
-              
-            return await strapi.api.vouchers.controllers.vouchers.GenerateVoucherWinner(options.user_id, options.plan_id);
-          }
-        },
-
-        // DeclareVoucherWinner: {
-        //     description: 'Declaring voucher winner',
-        //     policies: [],
-        //     resolverOf: 'application::vouchers.vouchers.find',
-        //     resolver: async (obj, options, ctx) => {
-        //       return await strapi.api.vouchers.controllers.vouchers.DeclareVoucherWinner(options.id);
-        //     }
-        // },
-        FinalizeWinner: {
-            description: 'complete the voucher process and publish the winnner',
-            policies: [],
-            resolverOf: 'application::vouchers.vouchers.find',
-            resolver: async (obj, options, ctx) => {
-              return await strapi.api.vouchers.controllers.vouchers.FinalizeWinner();
-            }
-        }
-        
-      },
-
-      Query: {
-        AvailableVouchers: {
-          description: 'Return the added vouchers',
-          resolverOf: 'application::vouchers.vouchers.find',
-          resolver: async (obj, options, ctx) => {
-            return await strapi.api.vouchers.controllers.vouchers.AvailableVouchers(options.where || {});
-          },
-        },
-      },
+  definition: `
+    type UserVoucher {
+      disabled: Boolean,
+      won: Boolean!,
+      voucher: Vouchers
     }
+    type AvailableVouchers {
+      vouchers: [Vouchers],
+      AvailedVouchers: [VoucherAvailed]!,
+    }
+  `,
+  mutation: `
+    GenerateVoucherWinner(user_id: Int!, plan_id: Int!): UserVoucher!,
+    FinalizeWinner: VoucherAvailed!,
+  `,
+  query: 'AvailableVouchers(where: JSON): AvailableVouchers!',
+  resolver: {
+    Mutation: {
+      GenerateVoucherWinner: {
+        description: 'Randomly selecting voucher winner with their respective plans',
+        policies: [],
+        resolverOf: 'application::vouchers.vouchers.find',
+        resolver: async (obj, options, ctx) => {
+
+          return await strapi.api.vouchers.controllers.vouchers.GenerateVoucherWinner(options.user_id, options.plan_id);
+        }
+      },
+      // DeclareVoucherWinner: {
+      //     description: 'Declaring voucher winner',
+      //     policies: [],
+      //     resolverOf: 'application::vouchers.vouchers.find',
+      //     resolver: async (obj, options, ctx) => {
+      //       return await strapi.api.vouchers.controllers.vouchers.DeclareVoucherWinner(options.id);
+      //     }
+      // },
+      FinalizeWinner: {
+        description: 'complete the voucher process and publish the winnner',
+        policies: [],
+        resolverOf: 'application::vouchers.vouchers.find',
+        resolver: async (obj, options, ctx) => {
+          return await strapi.api.vouchers.controllers.vouchers.FinalizeWinner();
+        }
+      }
+
+    },
+
+    Query: {
+      AvailableVouchers: {
+        description: 'Return the added vouchers',
+        resolverOf: 'application::vouchers.vouchers.find',
+        resolver: async (obj, options, ctx) => {
+          return await strapi.api.vouchers.controllers.vouchers.AvailableVouchers(options.where || {});
+        },
+      },
+    },
   }
+}
