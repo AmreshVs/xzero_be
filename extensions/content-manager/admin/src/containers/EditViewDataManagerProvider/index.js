6014a719e7f8ca08897f2bc1a2116154872abd7d
noufaldowgroup <noufaldowgroup@gmail.com>
2020-12-16 12:25:00 +0400

    index js

--- extensions/content-manager/admin/src/containers/EditViewDataManagerProvider/index.js
+++ extensions/content-manager/admin/src/containers/EditViewDataManagerProvider/index.js
@@ -411,10 +411,11 @@ const EditViewDataManagerProvider = ({
       }
 
       emitEvent(isCreatingEntry ? 'willCreateEntry' : 'willEditEntry');
-
-      let voucherDetails = await request('/vouchers/'+cleanedData.id, {
-        method: 'GET'
-      });
+      if (currentContentTypeLayout.apiID === 'vouchers') { 
+          let voucherDetails = await request('/vouchers/'+cleanedData.id, {
+            method: 'GET'
+          });
+      }
 
       try {
         // Time to actually send the data
@@ -479,8 +480,6 @@ const EditViewDataManagerProvider = ({
         //update the voucher status upon draw status change on strapi admin 
         if (currentContentTypeLayout.apiID === 'vouchers') { 
           
-          
-        
           if(voucherDetails.draw_status !== 'declare' && voucherDetails.draw_status !== 'publish' && (cleanedData.draw_status == 'declare' || cleanedData.draw_status == 'publish')) {
             const axios = require('axios');
             let confirmAct;
